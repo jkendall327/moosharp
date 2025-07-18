1a5e9e938957ede3a272e84b0b07a7e24151cec5
jackkendall <jkendall3096@gmail.com>
2025-07-18 20:29:58 +0100

    Tweaks to the server

--- MooSharp.Web/TelnetServer.cs
+++ MooSharp.Web/TelnetServer.cs
@@ -3,19 +3,21 @@ using System.Net.Sockets;
 
 namespace MooSharp.Web;
 
-public class TelnetServer(IServiceProvider serviceProvider) : BackgroundService
+public class TelnetServer(IServiceProvider serviceProvider, ILogger<TelnetServer> logger) : BackgroundService
 {
     private readonly List<PlayerConnection> _connections = new();
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var listener = new TcpListener(IPAddress.Any, 8888);
+        using var listener = new TcpListener(IPAddress.Any, 8888);
 
         listener.Start();
 
         while (!stoppingToken.IsCancellationRequested)
         {
             var client = await listener.AcceptTcpClientAsync(stoppingToken);
+            
+            logger.LogInformation("TCP client connected");
 
             // For each new connection, create a new scope to resolve services
             // This ensures each player connection has its own dependencies if needed
@@ -26,11 +28,17 @@ public class TelnetServer(IServiceProvider serviceProvider) : BackgroundService
                     // Resolve the shared world and a new player connection handler
                     var conn = new PlayerConnection(client);
 
+                    logger.LogInformation("Player connected");
+                    
                     _connections.Add(conn);
 
                     await conn.ProcessCommandsAsync();
+                    
+                    logger.LogInformation("Player disconnected");
                 },
                 stoppingToken);
         }
+        
+        logger.LogInformation("TCP server stopped");
     }
 }
