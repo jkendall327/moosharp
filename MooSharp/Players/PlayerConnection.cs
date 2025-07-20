df7667616ce3620157af23cf2e330f38ebec578b
jackkendall <jkendall3096@gmail.com>
2025-07-20 12:45:15 +0100

    Remove bad .csproj reference

--- MooSharp/Players/PlayerConnection.cs
+++ MooSharp/Players/PlayerConnection.cs
@@ -1,5 +1,4 @@
 using System.Text;
-using Microsoft.AspNetCore.SignalR;
 
 namespace MooSharp;
 
@@ -21,51 +20,3 @@ public interface IPlayerConnection
 }
 
 public record InputReceivedEvent(PlayerActor Player, string Input, CancellationToken Token);
-
-public class SignalRPlayerConnection : IPlayerConnection
-{
-    private readonly IHubContext<MooHub> _hubContext;
-    public string Id { get; }
-
-    public PlayerActor Player { get; }
-
-    public event Func<InputReceivedEvent, Task>? InputReceived;
-    public event Func<Task>? ConnectionLost;
-
-    public SignalRPlayerConnection(PlayerActor player, IHubContext<MooHub> hubContext, string connectionId)
-    {
-        Player = player;
-        Id = connectionId;
-        _hubContext = hubContext;
-    }
-
-    // Send a message to the specific client this connection represents
-    public Task SendMessageAsync(string message, CancellationToken cancellationToken = default)
-    {
-        // "ReceiveMessage" is the name of the method the Blazor client will be listening for.
-        return _hubContext.Clients.Client(Id).SendAsync("ReceiveMessage", message, cancellationToken);
-    }
-
-    public Task SendMessageAsync(StringBuilder message, CancellationToken cancellationToken = default)
-    {
-        // StringBuilder doesn't have a direct SendAsync overload, so we convert it.
-        return SendMessageAsync(message.ToString(), cancellationToken);
-    }
-    
-    public async Task OnInputReceivedAsync(string input)
-    {
-        if (InputReceived != null)
-        {
-            await InputReceived.Invoke(new(Player, input, CancellationToken.None));
-        }
-    }
-
-    // Called by the SignalR Hub when the client disconnects
-    public async Task OnConnectionLostAsync()
-    {
-        if (ConnectionLost != null)
-        {
-            await ConnectionLost.Invoke();
-        }
-    }
-}
