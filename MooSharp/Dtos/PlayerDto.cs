6e44b824bb59da6c76a4479d4d5c7353ff366abf
jackkendall <jkendall3096@gmail.com>
2025-11-23 15:15:08 +0000

    Dummy player save/load

--- MooSharp/Dtos/PlayerDto.cs
+++ MooSharp/Dtos/PlayerDto.cs
@@ -4,4 +4,5 @@ public class PlayerDto
 {
     public RoomId CurrentLocation { get; set; }
     public required string Username { get; set; }
+    public required string Password { get; set; }
 }
--- MooSharp/Persistence/IPlayerStore.cs
+++ MooSharp/Persistence/IPlayerStore.cs
@@ -8,13 +8,28 @@ public interface IPlayerStore
 
 public class JsonPlayerStore : IPlayerStore
 {
+    private readonly List<PlayerDto> _players = [new()
+    {
+        Username = "Jane Doe",
+        Password = "hunter123"
+    }];
+
     public Task SaveNewPlayer(Player player, string password)
     {
-        throw new NotImplementedException();
+        _players.Add(new()
+        {
+            Username = player.Username,
+            Password = password,
+            CurrentLocation = player.CurrentLocation.Id
+        });
+
+        return Task.CompletedTask;
     }
 
     public Task<PlayerDto?> LoadPlayer(LoginCommand command)
     {
-        throw new NotImplementedException();
+        var player = _players.SingleOrDefault(s => s.Username == command.Username && s.Password == command.Password);
+
+        return Task.FromResult(player);
     }
 }
