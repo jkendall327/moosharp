ca30281a42cf6e31ed3c39cb2fdb4fa22aa4ffb1
jackkendall <jkendall3096@gmail.com>
2025-11-23 12:39:44 +0000

    Slightly simplify

--- MooSharp/Commands/CommandExecutor.cs
+++ MooSharp/Commands/CommandExecutor.cs
@@ -8,28 +8,28 @@ public class CommandExecutor(IServiceProvider serviceProvider, ILogger<CommandEx
 {
     public async Task Handle(ICommand? cmd, StringBuilder sb, CancellationToken token = default)
     {
-        if (cmd is not null)
-        {
-            logger.LogDebug("Parsed input to command {CommandType}",  cmd.GetType().Name);
-            
-            // This switch expression is just here so the compiler determines the type of the cmd,
-            // and by extension, the generic argument to executor.Handle<T>().
-            // This lets the executor get the correct handler implementation from DI.
-            var task = cmd switch
-            {
-                ExamineCommand e => Handle(e, sb, token),
-                MoveCommand m => Handle(m, sb, token),
-                TakeCommand t => Handle(t, sb, token),
-                _ => throw new ArgumentOutOfRangeException(nameof(cmd), "Unrecognised command type")
-            };
-
-            await task;
-        }
-        else
+        if (cmd is null)
         {
             logger.LogDebug("Failed to parse player input as command");
             sb.AppendLine("That command wasn't recognized. Use 'move' to go between locations.");
+
+            return;
         }
+
+        logger.LogDebug("Parsed input to command {CommandType}", cmd.GetType().Name);
+
+        // This switch expression is just here so the compiler determines the type of the cmd,
+        // and by extension, the generic argument to executor.Handle<T>().
+        // This lets the executor get the correct handler implementation from DI.
+        var task = cmd switch
+        {
+            ExamineCommand e => Handle(e, sb, token),
+            MoveCommand m => Handle(m, sb, token),
+            TakeCommand t => Handle(t, sb, token),
+            _ => throw new ArgumentOutOfRangeException(nameof(cmd), "Unrecognised command type")
+        };
+
+        await task;
     }
 
     private async Task Handle<TCommand>(TCommand command, StringBuilder buffer, CancellationToken token = default)
