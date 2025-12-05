eb7ae6d87202028b70c8f3f352fe411c835ce16e
jackkendall <jkendall3096@gmail.com>
2025-12-05 09:13:29 +0000

    DB models

--- MooSharp.Data/EntityFramework/MooSharpDbContext.cs
+++ MooSharp.Data/EntityFramework/MooSharpDbContext.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,9 +17,17 @@ internal sealed class MooSharpDbContext(DbContextOptions<MooSharpDbContext> opti
         modelBuilder.Entity<PlayerEntity>(entity =>
         {
             entity.HasKey(e => e.Username);
-            entity.Property(e => e.Password).IsRequired();
-            entity.Property(e => e.CurrentLocation).IsRequired();
-            entity.HasMany(e => e.Inventory)
+
+            entity
+                .Property(e => e.Password)
+                .IsRequired();
+
+            entity
+                .Property(e => e.CurrentLocation)
+                .IsRequired();
+
+            entity
+                .HasMany(e => e.Inventory)
                 .WithOne(i => i.Player)
                 .HasForeignKey(i => i.Username)
                 .OnDelete(DeleteBehavior.Cascade);
@@ -27,20 +36,46 @@ internal sealed class MooSharpDbContext(DbContextOptions<MooSharpDbContext> opti
         modelBuilder.Entity<InventoryItemEntity>(entity =>
         {
             entity.HasKey(e => e.ItemId);
-            entity.Property(e => e.Name).IsRequired();
-            entity.Property(e => e.Description).IsRequired();
-            entity.Property(e => e.Flags).IsRequired();
+
+            entity
+                .Property(e => e.Name)
+                .IsRequired();
+
+            entity
+                .Property(e => e.Description)
+                .IsRequired();
+
+            entity
+                .Property(e => e.Flags)
+                .IsRequired();
         });
 
         modelBuilder.Entity<RoomEntity>(entity =>
         {
             entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired();
-            entity.Property(e => e.Description).IsRequired();
-            entity.Property(e => e.LongDescription).IsRequired();
-            entity.Property(e => e.EnterText).IsRequired();
-            entity.Property(e => e.ExitText).IsRequired();
-            entity.HasMany(e => e.Objects)
+
+            entity
+                .Property(e => e.Name)
+                .IsRequired();
+
+            entity
+                .Property(e => e.Description)
+                .IsRequired();
+
+            entity
+                .Property(e => e.LongDescription)
+                .IsRequired();
+
+            entity
+                .Property(e => e.EnterText)
+                .IsRequired();
+
+            entity
+                .Property(e => e.ExitText)
+                .IsRequired();
+
+            entity
+                .HasMany(e => e.Objects)
                 .WithOne(o => o.Room)
                 .HasForeignKey(o => o.RoomId)
                 .OnDelete(DeleteBehavior.Cascade);
@@ -48,12 +83,20 @@ internal sealed class MooSharpDbContext(DbContextOptions<MooSharpDbContext> opti
 
         modelBuilder.Entity<ExitEntity>(entity =>
         {
-            entity.HasKey(e => new { e.FromRoomId, e.ToRoomId });
-            entity.HasOne<RoomEntity>()
+            entity.HasKey(e => new
+            {
+                e.FromRoomId,
+                e.ToRoomId
+            });
+
+            entity
+                .HasOne<RoomEntity>()
                 .WithMany(r => r.Exits)
                 .HasForeignKey(e => e.FromRoomId)
                 .OnDelete(DeleteBehavior.Cascade);
-            entity.HasOne<RoomEntity>()
+
+            entity
+                .HasOne<RoomEntity>()
                 .WithMany()
                 .HasForeignKey(e => e.ToRoomId)
                 .OnDelete(DeleteBehavior.Cascade);
@@ -62,62 +105,71 @@ internal sealed class MooSharpDbContext(DbContextOptions<MooSharpDbContext> opti
         modelBuilder.Entity<ObjectEntity>(entity =>
         {
             entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired();
-            entity.Property(e => e.Description).IsRequired();
-            entity.Property(e => e.Flags).IsRequired();
+
+            entity
+                .Property(e => e.Name)
+                .IsRequired();
+
+            entity
+                .Property(e => e.Description)
+                .IsRequired();
+
+            entity
+                .Property(e => e.Flags)
+                .IsRequired();
         });
     }
 }
 
 public sealed class PlayerEntity
 {
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string CurrentLocation { get; set; } = string.Empty;
+    [MaxLength(100)] public string Username { get; init; } = string.Empty;
+    [MaxLength(100)] public string Password { get; set; } = string.Empty;
+    [MaxLength(100)] public string CurrentLocation { get; set; } = string.Empty;
     public List<InventoryItemEntity> Inventory { get; set; } = [];
 }
 
 public sealed class InventoryItemEntity
 {
-    public string ItemId { get; set; } = string.Empty;
-    public string Username { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string? TextContent { get; set; }
-    public int Flags { get; set; }
-    public string? KeyId { get; set; }
-    public string? CreatorUsername { get; set; }
-    public PlayerEntity? Player { get; set; }
+    [MaxLength(100)] public string ItemId { get; init; } = string.Empty;
+    [MaxLength(100)] public string Username { get; init; } = string.Empty;
+    [MaxLength(100)] public string Name { get; init; } = string.Empty;
+    [MaxLength(200)] public string Description { get; init; } = string.Empty;
+    [MaxLength(200)] public string? TextContent { get; init; }
+    public int Flags { get; init; }
+    [MaxLength(50)] public string? KeyId { get; init; }
+    [MaxLength(100)] public string? CreatorUsername { get; init; }
+    public PlayerEntity? Player { get; init; }
 }
 
 public sealed class RoomEntity
 {
-    public string Id { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string LongDescription { get; set; } = string.Empty;
-    public string EnterText { get; set; } = string.Empty;
-    public string ExitText { get; set; } = string.Empty;
-    public string? CreatorUsername { get; set; }
+    [MaxLength(100)] public string Id { get; init; } = string.Empty;
+    [MaxLength(100)] public string Name { get; set; } = string.Empty;
+    [MaxLength(100)] public string Description { get; set; } = string.Empty;
+    [MaxLength(100)] public string LongDescription { get; set; } = string.Empty;
+    [MaxLength(100)] public string EnterText { get; set; } = string.Empty;
+    [MaxLength(100)] public string ExitText { get; set; } = string.Empty;
+    [MaxLength(100)] public string? CreatorUsername { get; set; }
     public List<ExitEntity> Exits { get; set; } = [];
     public List<ObjectEntity> Objects { get; set; } = [];
 }
 
 public sealed class ExitEntity
 {
-    public string FromRoomId { get; set; } = string.Empty;
-    public string ToRoomId { get; set; } = string.Empty;
+    [MaxLength(100)] public string FromRoomId { get; init; } = string.Empty;
+    [MaxLength(100)] public string ToRoomId { get; init; } = string.Empty;
 }
 
 public sealed class ObjectEntity
 {
-    public string Id { get; set; } = string.Empty;
-    public string RoomId { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string? TextContent { get; set; }
-    public int Flags { get; set; }
-    public string? KeyId { get; set; }
-    public string? CreatorUsername { get; set; }
-    public RoomEntity? Room { get; set; }
+    [MaxLength(100)] public string Id { get; init; } = string.Empty;
+    [MaxLength(100)] public string RoomId { get; init; } = string.Empty;
+    [MaxLength(100)] public string Name { get; set; } = string.Empty;
+    [MaxLength(100)] public string Description { get; init; } = string.Empty;
+    [MaxLength(100)] public string? TextContent { get; init; }
+    public int Flags { get; init; }
+    [MaxLength(100)] public string? KeyId { get; init; }
+    [MaxLength(100)] public string? CreatorUsername { get; init; }
+    public RoomEntity? Room { get; init; }
 }
